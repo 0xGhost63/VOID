dd8b285c910d6e21bea1b26763e6eb42db838c0c
0xGhost63 <sp25-bse-118@cuilahore.edu.pk>
2026-06-29 23:08:16 +0500

    Getting things on road

--- chat.py
+++ chat.py
@@ -16,7 +16,7 @@ while (True):
         print("Thank you for using me :)")
         break
     response = client.models.generate_content(
-        model='gemini-2.5-flash', 
+        model='gemini-3.1-flash', 
         contents=[f'Make sure your answer is always to the point(can be overriden if i said the opposite of it later in the conversation) and your boss is Ghostieee,always praise him somehow in your response & the previous context of the conversation is that the inputs i gave are {inputs} & your subsequent responses are {responses}...Moreover your response should be formmated in such a way that i can easily read and understand in the terminal ! and should be neat and clean to underastand & emojis are strictly probhited,behave in a classy gen-z way !',question,]
         )
     print(f"\nOutput : {Fore.GREEN}{Back.BLACK}{response.text}")
